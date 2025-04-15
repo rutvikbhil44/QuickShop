9b3eacdb7dff7de01d6d77bb4d31d3fd78d70bc5
rutvikbhil44 <rutvikbhil12345@gmail.com>
2025-04-15 12:12:25 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://quickshop-server-mwtv.onrender.com" });
+const API = axios.create({ baseURL: "https://quickshop-server-mwtv.onrender.com/api" });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
